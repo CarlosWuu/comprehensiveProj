d798b4d09c7ccbe7674f9530b63fc7202d13d679
CarlosWuu <mricybin@163.com>
2018-05-21 09:57:19 +0800

    Create BarcodeUtil.java

---
+++ commons/src/main/java/com/wuu/tech/commons/barcode/BarcodeUtil.java
@@ -0,0 +1,90 @@
+package com.wuu.tech.commons.barcode;
+
+import org.apache.commons.lang.StringUtils;
+import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
+import org.krysalis.barcode4j.tools.UnitConv;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+/**
+ * Created by carlos wuu on 2018/5/17.
+ * 条形码生成器
+ */
+public class BarcodeUtil {
+    /**
+     * 生成文件
+     *
+     * @param msg
+     * @param path
+     * @return
+     */
+    public static File generateFile(String msg, String path) {
+        File file = new File(path);
+        try {
+            generate(msg, new FileOutputStream(file));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return file;
+    }
+
+    /**
+     * 生成字节
+     *
+     * @param msg
+     * @return
+     */
+    public static byte[] generate(String msg) {
+        ByteArrayOutputStream ous = new ByteArrayOutputStream();
+        generate(msg, ous);
+        return ous.toByteArray();
+    }
+
+    /**
+     * 生成到流
+     *
+     * @param msg
+     * @param ous
+     */
+    public static void generate(String msg, OutputStream ous) {
+        if (StringUtils.isEmpty(msg) || ous == null) {
+            return;
+        }
+
+        MyCode39Bean bean = new MyCode39Bean();
+
+        // 精细度
+        final int dpi = 100;
+        // module宽度
+        final double moduleWidth = UnitConv.in2mm(1.0f / dpi);
+
+        // 配置对象
+        bean.setModuleWidth(moduleWidth);
+        bean.setWideFactor(3);
+        bean.doQuietZone(false);
+
+
+        String format = "image/png";
+        try {
+
+            // 输出到流
+            BitmapCanvasProvider canvas = new BitmapCanvasProvider(ous, format, dpi,
+                    BufferedImage.TYPE_BYTE_BINARY, false, 0);
+
+            // 生成条形码
+            bean.generateBarcode(canvas, msg);
+
+            // 结束绘制
+            canvas.finish();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String msg = " 3773306";
+        String path = "D:\\barcode.png";
+        generateFile(msg, path);
+    }
+}
